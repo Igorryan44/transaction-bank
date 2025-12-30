f0e9dbb855c878ee9c563df212f3bdf57fda44d7
Igorryan44 <igorryanmendesalves>
2025-12-29 22:01:48 -0300

    Desafio backend transaction-bank

---
+++ transaction-bank/src/main/java/com/igor_dev/transaction_bank/TransactionBankApplication.java
@@ -0,0 +1,13 @@
+package com.igor_dev.transaction_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionBankApplication.class, args);
+	}
+
+}
---
+++ transaction-bank/src/test/java/com/igor_dev/transaction_bank/TransactionBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor_dev.transaction_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
